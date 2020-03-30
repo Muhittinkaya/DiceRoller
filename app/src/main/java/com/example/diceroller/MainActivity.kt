d8b5b64c07254f6d0f028d8c0bf33716cde640b0
Muhittinkaya <mhttnkaya@gmail.com>
2020-03-30 18:02:59 +0300

    Dice Rolling Android application.

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.diceroller
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage: ImageView
+    lateinit var diceImage2: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+        diceImage = findViewById(R.id.dice_image)
+        diceImage2 = findViewById(R.id.dice_image2)
+    }
+
+    private fun rollDice() {
+
+        val randomDice = Random().nextInt(6) + 1
+        val randomDice2 = Random().nextInt(6) + 1
+
+        val drawableResource = when(randomDice){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+
+        val drawableResource2 = when(randomDice2){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage2.setImageResource(drawableResource2)
+    }
+}
